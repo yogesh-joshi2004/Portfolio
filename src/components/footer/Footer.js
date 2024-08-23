fe212885f23d2a084b136e6b0de42559254279cc
yogesh-joshi2004 <yogij2453@gmail.com>
2024-08-23 21:36:20 +0530

    icon

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 import './footer.css';
 
 
-const Footer=()=>{
+const Footer = () => {
 
 
-return(
+  return (
 
     <footer className="footer">
       <div className="footer-box">
@@ -16,13 +16,13 @@ return(
         <p className="footer-pera">Follow me on</p>
       </div>
       <div className="footer-icon">
-        <div className="footer-icon-1"><a href="https://instagram.com"><img src="./instagram-brands-solid.svg" alt="insta" /> </a></div>
+        <div className="footer-icon-1"><a href="https://leetcode.com/u/yogeshJoshi2004"><img src="./instagram-brands-solid.svg" alt="insta" /> </a></div>
         <div className="footer-icon-1"><a href="https://github.com/yogesh-joshi2004"><img src="./github-brands-solid.svg" alt="github" /></a></div>
         <div className="footer-icon-1"><a href="https://www.linkedin.com/in/yogesh-joshi-b1a6822a6" ><img src="./linkedin-brands-solid.svg" alt="linkedin" /></a> </div>
       </div>
     </footer>
 
-   
-)
+
+  )
 }
 export default Footer;
